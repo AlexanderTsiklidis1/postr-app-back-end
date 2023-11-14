cf0ef0a710f6ca685e45f9f114347a9aaaf07a87
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-14 17:32:47 -0500

    puts in more data into project

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,18 @@
 
 INSERT INTO posts (id, username, postMessage, post_pic, profile_pic)
 VALUES
-  ('0','ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRzPqYCsL1DMmMCTEBEw23FkY1F8XxcY2ibyS1CcgWvM8e0IY2O26rOHr1omuv55qsNgLhIlwBGn4ZGwscBVIiRIN3knWzeFSZObJkP7LedZMIE_G5fpSdQDTNcBoenGnP6aLlq8sQ&usqp=CAc', 'https://avatars.githubusercontent.com/u/129804495?v=4'),
-  ('1', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '','https://avatars.githubusercontent.com/u/10932336?v=4')
-
-  
+  ('0', 'ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRzPqYCsL1DMmMCTEBEw23FkY1F8XxcY2ibyS1CcgWvM8e0IY2O26rOHr1omuv55qsNgLhIlwBGn4ZGwscBVIiRIN3knWzeFSZObJkP7LedZMIE_G5fpSdQDTNcBoenGnP6aLlq8sQ&usqp=CAc', 'https://avatars.githubusercontent.com/u/129804495?v=4'),
+  ('1', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '', 'https://avatars.githubusercontent.com/u/10932336?v=4'),
+  ('2', 'CodingNinja', 'Just finished a marathon coding session! 💻 #CodeLife', 'https://example.com/coding_pic.jpg', 'https://avatars.githubusercontent.com/u/98765432?v=4'),
+  ('3', 'AdventureSeeker', 'Exploring new horizons! 🌍 #TravelBug', 'https://example.com/travel_pic.jpg', 'https://avatars.githubusercontent.com/u/87654321?v=4'),
+  ('4', 'FoodieExplorer', 'Tried a new recipe today - absolutely delicious! 🍲 #Foodie', 'https://example.com/food_pic.jpg', 'https://avatars.githubusercontent.com/u/76543210?v=4'),
+  ('5', 'GamerPro', 'Just achieved a new high score in my favorite game! 🎮 #GamerLife', 'https://example.com/gaming_pic.jpg', 'https://avatars.githubusercontent.com/u/65432109?v=4'),
+  ('6', 'FitnessFanatic', 'Morning workout complete! 💪 #FitnessGoals', 'https://example.com/fitness_pic.jpg', 'https://avatars.githubusercontent.com/u/54321098?v=4'),
+  ('7', 'Bookworm', 'Finished reading an amazing book today. Highly recommend! 📚 #BookLover', 'https://example.com/book_pic.jpg', 'https://avatars.githubusercontent.com/u/43210987?v=4'),
+  ('8', 'ArtisticSoul', 'Created a beautiful piece of art. What do you think? 🎨 #ArtLife', 'https://example.com/art_pic.jpg', 'https://avatars.githubusercontent.com/u/32109876?v=4'),
+  ('9', 'MusicMaestro', 'Jamming to my favorite tunes! 🎶 #MusicLover', 'https://example.com/music_pic.jpg', 'https://avatars.githubusercontent.com/u/21098765?v=4'),
+  ('10', 'TechEnthusiast', 'Excited about the latest tech trends. Anyone else? 🚀 #Techie', 'https://example.com/tech_pic.jpg', 'https://avatars.githubusercontent.com/u/10987654?v=4'),
+  ('11', 'NatureLover', 'Spent the day surrounded by nature. Feeling refreshed! 🌳 #Nature', 'https://example.com/nature_pic.jpg', 'https://avatars.githubusercontent.com/u/98765432?v=4'),
+  ('12', 'Fashionista', 'New fashion find! 👗 #FashionForward', 'https://example.com/fashion_pic.jpg', 'https://avatars.githubusercontent.com/u/87654321?v=4'),
+  ('13', 'DIYMaster', 'Completed a DIY project. Proud of the results! 🔨 #DIY', 'https://example.com/diy_pic.jpg', 'https://avatars.githubusercontent.com/u/76543210?v=4'),
+  ('14', 'MotivationMaven', 'Remember: You are capable of amazing things! 💪 #Motivation', 'https://example.com/motivation_pic.jpg', 'https://avatars.githubusercontent.com/u/65432109?v=4');
