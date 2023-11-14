88d9f2794e7e9c3233b6c19ecec7b74b1e90ed89
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-14 15:01:47 -0500

    changes from songs_dev to posts_dev in env file

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO posts (id, username, postMessage, post_pic, profile_pic)
 VALUES
-  ('1','ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', '', ''),
-  ('2', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '','')
+  ('0','ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', '', ''),
+  ('1', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '','')
 
   
