9f09fcfe8a38b1a752aaed0896e7410e382c4c65
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-14 15:09:22 -0500

    adds pics to first two seeds

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO posts (id, username, postMessage, post_pic, profile_pic)
 VALUES
-  ('0','ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', '', ''),
-  ('1', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '','')
+  ('0','ATsiklidis', 'Hi, I am the first to ever post on the POSTR app. Firsts', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRzPqYCsL1DMmMCTEBEw23FkY1F8XxcY2ibyS1CcgWvM8e0IY2O26rOHr1omuv55qsNgLhIlwBGn4ZGwscBVIiRIN3knWzeFSZObJkP7LedZMIE_G5fpSdQDTNcBoenGnP6aLlq8sQ&usqp=CAc', 'https://avatars.githubusercontent.com/u/129804495?v=4'),
+  ('1', 'SungY', 'Hi, I am the second ever to post on POSTR! WOOT WOOT!', '','https://avatars.githubusercontent.com/u/10932336?v=4')
 
   
